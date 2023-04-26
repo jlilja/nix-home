87c116a183fe07f2fb61b727915ab9adf56ff078
Jonas Lilja <jonas@lilja.io>
2023-04-26 10:37:52 +0200

    Move cli pkgs into home.

--- jonas/home.nix
+++ jonas/home.nix
@@ -4,7 +4,6 @@
   imports = [
     ./firefox.nix
     ./neovim.nix
-    ./cli.nix
     ./git.nix
     ./ssh.nix
     ./zsh.nix
@@ -35,9 +34,14 @@
     # Editing
     sublime4
 
-    # CLI related
+    # CLI
     zsh
     nix # Fix for nix to make sure it gets added to $PATH when zsh isn't the the initial shell.
+    gping
+    htop
+    jq
+    fzf # https://github.com/junegunn/fzf
+    dive # https://github.com/wagoodman/dive
 
     # Chat apps
     teams-for-linux # Open-Source Teams Electron App (https://github.com/IsmaelMartinez/teams-for-linux)
