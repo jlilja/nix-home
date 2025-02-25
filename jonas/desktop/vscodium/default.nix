2db7ed66da7ef9c9708d4a3c13f0d0ba82b2b303
Jonas Lilja <jonas@lilja.io>
2025-02-25 22:14:48 +0100

    Flake update. Add fixes for vscode

--- jonas/desktop/vscodium/default.nix
+++ jonas/desktop/vscodium/default.nix
@@ -1,71 +1,73 @@
 { config, pkgs, ... }:
 
 {
-  programs.vscode.profiles = {
+  programs.vscode = {
     enable = true;
 
     package = pkgs.vscodium;
 
-    userSettings = {
-      files = {
-        trimFinalNewlines = true;
-        insertFinalNewline = true;
-        trimTrailingWhitespace = true;
-      };
+    profiles.default = {
+      userSettings = {
+        files = {
+          trimFinalNewlines = true;
+          insertFinalNewline = true;
+          trimTrailingWhitespace = true;
+        };
 
-      workbench = {
-        startupEditor = "None";
-      };
+        workbench = {
+          startupEditor = "None";
+        };
 
-      editor = {
-        mouseWheelZoom = true;
-        formatOnSave = true;
-      };
-
-      extensions = {
-        autoCheckUpdates = false;
-        autoUpdate = false;
-      };
-
-      telemetry.telemetryLevel = "off";
-      redhat.telemetry.enabled = false;
-
-      gitlens = {
-        telemetry.enabled = false;
-        codeLens.enabled = false;
-      };
-
-      "[yaml]".editor.defaultFormatter = "redhat.vscode-yaml";
-
-      "[terraform]" = {
         editor = {
-          defaultFormatter = "hashicorp.terraform";
-          formatOnSave = false;
+          mouseWheelZoom = true;
+          formatOnSave = true;
         };
-        "editor.codeActionsOnSave" = {
-          "source.formatAll.terraform" = "explicit";
+
+        extensions = {
+          autoCheckUpdates = false;
+          autoUpdate = false;
         };
+
+        telemetry.telemetryLevel = "off";
+        redhat.telemetry.enabled = false;
+
+        gitlens = {
+          telemetry.enabled = false;
+          codeLens.enabled = false;
+        };
+
+        "[yaml]".editor.defaultFormatter = "redhat.vscode-yaml";
+
+        "[terraform]" = {
+          editor = {
+            defaultFormatter = "hashicorp.terraform";
+            formatOnSave = false;
+          };
+          "editor.codeActionsOnSave" = {
+            "source.formatAll.terraform" = "explicit";
+          };
+        };
+
+        terraform.languageServer.path = "${pkgs.terraform-ls}/bin/terraform-ls";
+
+        ansible = {
+          ansible.path = "${pkgs.ansible}/bin/ansible";
+          python.interpreterPath = "${pkgs.python3}/bin";
+        };
+
       };
+    
+      extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace (
+        builtins.fromJSON (
+          builtins.readFile ./extensions.json
+        )
+      );
 
-      terraform.languageServer.path = "${pkgs.terraform-ls}/bin/terraform-ls";
-
-      ansible = {
-        ansible.path = "${pkgs.ansible}/bin/ansible";
-        python.interpreterPath = "${pkgs.python3}/bin";
-      };
-
+      keybindings = with builtins; fromJSON (
+        readFile ./keybindings.json
+      );
     };
 
-    keybindings = with builtins; fromJSON (
-      readFile ./keybindings.json
-    );
-
     mutableExtensionsDir = false;
-
-    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace (
-      builtins.fromJSON (
-        builtins.readFile ./extensions.json
-      )
-    );
   };
 }
